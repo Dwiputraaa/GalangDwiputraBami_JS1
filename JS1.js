89a48048e243a2251c5727dacc5ebd7ca46d42f1
unknown <dwiputra9253@gmail.com>
2023-10-09 00:18:12 -1200

    first commit

---
+++ JS1.js
@@ -0,0 +1,124 @@
+// //if else
+
+var angka = 9;
+if (angka % 2 == 0){
+    console.log(angka + ' ' + 'adalah bilangan genap')
+}
+else{
+    console.log(angka + ' ' + 'adalah bilangan ganjil')
+}
+
+
+// //nested if 
+
+// var age = 10; 
+// var memiliki_sim = false; 
+
+// if (age >= 18) {
+//   console.log("Anda bisa membuat SIM");
+//   if (memiliki_sim) {
+//     console.log("Anda bisa membawa kendaraan");
+//   } else {
+//     console.log("Anda tidak bisa membawa kendaraan");
+//   }
+// } else {
+//   console.log("Anda tidak bisa membuat SIM");
+// }
+
+
+// //swith case
+
+// var cuaca = "";
+
+// switch (cuaca) {
+//   case "Salju":
+//     console.log("Ini Musim  Salju");
+//     break;
+//   case "Hujan":
+//     console.log("Ini Musim Hujan");
+//     break;
+//   case "Kemarau":
+//     console.log("Ini Musim Kemarau");
+//     break;
+//   case "Gugur":
+//     console.log("Ini Musim Gugur");
+//     break;
+//   default:
+//     console.log("Ini Hari yang Cerah");
+// }
+
+
+// //for
+
+// for (var a = 0; a < 4; a++ ){
+//     console.log('Galang');
+// }
+
+
+// //While
+
+// var hitung = 0;
+
+// while (hitung < 5) {
+//   console.log(hitung);
+//   hitung++;
+// }
+
+
+// //do...while
+
+// var hitung = 100;
+
+// do {
+//   console.log(hitung);
+//   hitung++;
+// } while (hitung <= 110);
+
+
+// //Build Function
+
+// function cuaca(){
+//     var kondisi = 'gelap';
+//     if (kondisi === 'cerah'){
+//     console.log('saya akan pergi ke kampus')
+//     } else{
+//     console.log('saya tidak jadi pergi ke kampus')
+//     }
+// }
+// cuaca();
+
+
+// //Function Parameter
+
+// function cuaca(nama, cuaca) {
+//     var cuaca = 'cerah';
+//     if (cuaca === 'cerah') {
+//       console.log(nama + ' akan pergi ke kampus');
+//     } else {
+//       console.log(nama + ' tidak jadi pergi ke kampus');
+//     }
+// }
+// cuaca('Galang', cuaca);
+
+
+
+
+// // Function Return 
+
+// function getStatusDosen(dosen) {
+//     switch (dosen) {
+//       case 'hadir':
+//         return 'Saya akan masuk kelas';
+//       case 'berhalangan':
+//         return 'Saya tidak akan masuk kelas';
+//     }
+//   }
+  
+//   var dosen = "hadir";
+//   var statusDosen = getStatusDosen(dosen); 
+//   console.log(statusDosen); 
+   
+  
+ 
+  
+
